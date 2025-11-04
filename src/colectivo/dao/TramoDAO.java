e6139ca5a6e1fabcf484ec4e4013b18f6b923ba5
Agustin Guillermo Gigliotti Farias <agustingigliottifarias@gmail.com>
2025-11-04 18:48:06 -0300

    Delete src directory

--- src/colectivo/dao/TramoDAO.java
+++
@@ -1,9 +0,0 @@
-package colectivo.dao;
-
-import colectivo.modelo.Tramo;
-import java.util.Map;
-
-public interface TramoDAO {
-    /** Clave requerida por la cátedra: "origenId-destinoId" (ej: "66-31"). */
-    Map<String, Tramo> buscarTodos();
-}
