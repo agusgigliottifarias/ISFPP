12f0781af95c6269c5f5adcc71e956cdbf5e9c17
Agustin Guillermo Gigliotti Farias <agustingigliottifarias@gmail.com>
2025-11-05 15:52:10 -0300

    Delete src directory
    borrar desactualizado

--- src/colectivo/dao/FrecuenciaDAO.java
+++
@@ -1,8 +0,0 @@
-package colectivo.dao;
-
-import colectivo.modelo.Frecuencia;
-import java.util.List;
-
-public interface FrecuenciaDAO {
-    List<Frecuencia> buscarTodos();
-}
--- src/colectivo/dao/LineaDAO.java
+++
@@ -1,9 +0,0 @@
-package colectivo.dao;
-
-import colectivo.modelo.Linea;
-import java.util.Map;
-
-public interface LineaDAO {
-    /** Clave sugerida: String.valueOf(idLinea). */
-    Map<String, Linea> buscarTodos();
-}
