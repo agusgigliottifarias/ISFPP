78b311f58a773c22d88d67ea720cb530dc35d8df
Agustin Guillermo Gigliotti Farias <agustingigliottifarias@gmail.com>
2025-11-05 23:15:16 -0300

    Delete src directory

--- src/colectivo/dao/ParadaDAO.java
+++
@@ -1,9 +0,0 @@
-package colectivo.dao;
-
-import colectivo.modelo.Parada;
-import java.util.Map;
-
-public interface ParadaDAO {
-    /** Clave sugerida: String.valueOf(idParada). */
-    Map<Integer, Parada> buscarTodos();
-}
